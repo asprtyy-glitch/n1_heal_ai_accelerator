bdd2c3fee5f97617e63d58d2a92d3f890d75ddbf
asprtyy-glitch <asprtyy@gmail.com>
2026-04-26 21:56:57 +0300

    Update user_project_wrapper.v

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -1,51 +1,14 @@
-// SPDX-FileCopyrightText: 2020 Efabless Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// SPDX-License-Identifier: Apache-2.0
-
 `default_nettype none
-/*
- *-------------------------------------------------------------
- *
- * user_project_wrapper
- *
- * This wrapper enumerates all of the pins available to the
- * user for the user project.
- *
- * An example user project is provided in this wrapper.  The
- * example should be removed and replaced with the actual
- * user project.
- *
- *-------------------------------------------------------------
- */
 
-module user_project_wrapper #(
-    parameter BITS = 32
-) (
+module user_project_wrapper (
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 analog ground
-    inout vssa2,	// User area 2 analog ground
     inout vccd1,	// User area 1 1.8V supply
-    inout vccd2,	// User area 2 1.8v supply
     inout vssd1,	// User area 1 digital ground
-    inout vssd2,	// User area 2 digital ground
 `endif
-
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
     input wb_rst_i,
+
     input wbs_stb_i,
     input wbs_cyc_i,
     input wbs_we_i,
@@ -65,59 +28,51 @@ module user_project_wrapper #(
     output [`MPRJ_IO_PADS-1:0] io_out,
     output [`MPRJ_IO_PADS-1:0] io_oeb,
 
-    // Analog (direct connection to GPIO pad---use with caution)
-    // Note that analog I/O is not available on the 7 lowest-numbered
-    // GPIO pads, and so the analog_io indexing is offset from the
-    // GPIO indexing by 7 (also upper 2 GPIOs do not have analog_io).
+    // Analog (not used)
     inout [`MPRJ_IO_PADS-10:0] analog_io,
 
-    // Independent clock (on independent integer divider)
-    input   user_clock2,
+    // Independent Clock (not used)
+    input user_clock2,
 
     // User maskable interrupt signals
     output [2:0] user_irq
 );
 
-/*--------------------------------------*/
-/* User project is instantiated  here   */
-/*--------------------------------------*/
+    // Internal Reset Signal
+    wire rst_n = ~wb_rst_i;
 
-user_proj_example mprj (
-`ifdef USE_POWER_PINS
-	.vccd1(vccd1),	// User area 1 1.8V power
-	.vssd1(vssd1),	// User area 1 digital ground
-`endif
+    // N1-HEAL Top Instance
+    n1_heal_top #(
+        .NUM_CORES(16),
+        .DATA_WIDTH(512)
+    ) uut (
+    `ifdef USE_POWER_PINS
+        .vccd1(vccd1),
+        .vssd1(vssd1),
+    `endif
+        .clk                (wb_clk_i),
+        .rst_n              (rst_n),
+        .wb_stb_i           (wbs_stb_i),
+        .wb_cyc_i           (wbs_cyc_i),
+        .wb_we_i            (wbs_we_i),
+        .wb_sel_i           (wbs_sel_i),
+        .wb_dat_i           (wbs_dat_i),
+        .wb_adr_i           (wbs_adr_i),
+        .wb_ack_o           (wbs_ack_o),
+        .wb_dat_o           (wbs_dat_o),
+        .wb_err_o           (), // Unused error signal
+        .irq_done_o         (user_irq[0]),
+        .irq_thermal_o      (user_irq[1]),
+        .liquid_temp_sensor (la_data_in[7:0]), // Input via Logic Analyzer
+        .pump_ctrl          (io_out[7:0]),     // Output via GPIOs
+        .thermal_crit_flag_o(user_irq[2]),
+        .sleep_mode_i       (la_data_in[8]),
+        .busy_o             (la_data_out[9])
+    );
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
+    // GPIO configuration
+    assign io_oeb[7:0] = 8'b00000000; // Set as outputs
+    assign io_oeb[`MPRJ_IO_PADS-1:8] = {(`MPRJ_IO_PADS-8){1'b1}}; // Set others as inputs
 
-    // MGMT SoC Wishbone Slave
-
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
-
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
-    // IO Pads
-
-    .io_in ({io_in[37:30],io_in[7:0]}),
-    .io_out({io_out[37:30],io_out[7:0]}),
-    .io_oeb({io_oeb[37:30],io_oeb[7:0]}),
-
-    // IRQ
-    .irq(user_irq)
-);
-
-endmodule	// user_project_wrapper
-
+endmodule
 `default_nettype wire
